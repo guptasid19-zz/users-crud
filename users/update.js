7e51e4a6ed4ebb66f8aca95b41505f8721986f0a
Siddhartha Gupta <siddharth@travelstop.com>
2019-12-27 00:28:20 +0530

    Added function for updating user data

---
+++ users/update.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.update = async (event, context) => {
+
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: 'sls-users',
+    Key: {
+      id: event.pathParameters.id,
+    },
+    ExpressionAttributeValues: {
+      ':username': data.username,
+      ':email': data.email,
+      ':updatedAt': timestamp,
+    },
+    UpdateExpression: 'SET username = :username, email = :email, updatedAt = :updatedAt',
+    ReturnValues: 'ALL_NEW',
+  };
+
+  try {
+    const result = await dynamoDb.update(params).promise();
+    // console.log(result);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+        statusCode: 500,
+        body: 'Couldn\'t update the user.',
+    };
+  }
+};
